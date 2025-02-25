e6335f1c1e0ebcc3f641cdca97abd48d88711362
madoche <madochelj@gmail.com>
2025-02-24 23:55:23 -0500

    last commit of the night

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ def main():
     #print(f"Screen width: {SCREEN_WIDTH}")
     #print(f"Screen height: {SCREEN_HEIGHT}")
     
-    white = (255,255,255)
+    pygame.init()
+    black = (0,0,0)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        pygame.init()
         screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        screen.fill(white)
+        #screen.fill(black)
         pygame.display.flip()
 
 if __name__ == "__main__":
